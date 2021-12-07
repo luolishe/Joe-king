fc9e1161acd291cb73f6b7c1c7db6b68ecdda6a2
洛璃社 <xswluo@gmail.com>
2021-12-07 21:24:28 +0800

    Update yyapi.php

--- yiyan/yyapi.php
+++ yiyan/yyapi.php
@@ -1,6 +1,6 @@
 <?php
 // 存储数据的文件
-$filename = 'jcyulu.dat';        
+$filename = 'https://cdn.jsdelivr.net/gh/luolishe/Joe-king@main/yiyan/jcyulu.dat';        
  
 // 指定页面编码
 header('Content-type: text/html; charset=utf-8');
