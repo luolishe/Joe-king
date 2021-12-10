3c623f48c673bdc2dc8363c7381e95bc385fa969
洛璃社 <xswluo@gmail.com>
2021-12-10 22:36:35 +0800

    美化控制台
    美化控制台

---
+++ themes/BeautifyConsole/BeautifyConsole.js
@@ -0,0 +1,46 @@
+/* 自定义Console */
+    Function.prototype.getMultiLine = function () {
+            var lines = new String(this);
+            lines = lines.substring(lines.indexOf("/*") + 3, lines.lastIndexOf("*/"));
+            return lines;
+        }
+         var string = function () {
+/*
+
+          _____                _____                    _____          
+         /\    \              |\    \                  /\    \         
+        /::\    \             |:\____\                /::\____\        
+       /::::\    \            |::|   |               /:::/    /        
+      /::::::\    \           |::|   |              /:::/    /         
+     /:::/\:::\    \          |::|   |             /:::/    /          
+    /:::/  \:::\    \         |::|   |            /:::/    /           
+   /:::/    \:::\    \        |::|   |           /:::/    /            
+  /:::/    / \:::\    \       |::|___|______    /:::/    /      _____  
+ /:::/    /   \:::\    \      /::::::::\    \  /:::/____/      /\    \ 
+/:::/____/     \:::\____\    /::::::::::\____\|:::|    /      /::\____\
+\:::\    \      \::/    /   /:::/~~~~/~~      |:::|____\     /:::/    /
+ \:::\    \      \/____/   /:::/    /          \:::\    \   /:::/    / 
+  \:::\    \              /:::/    /            \:::\    \ /:::/    /  
+   \:::\    \            /:::/    /              \:::\    /:::/    /   
+    \:::\    \           \::/    /                \:::\__/:::/    /    
+     \:::\    \           \/____/                  \::::::::/    /     
+      \:::\    \                                    \::::::/    /      
+       \:::\____\                                    \::::/    /       
+        \::/    /                                     \::/____/        
+         \/____/                                       ~~              
+                                                                       
+
+*/
+        }
+      window.console.log(string.getMultiLine());
+      window.console.log("他们都说时光太瘦，指缝太宽");
+	window.console.log("所以很多东西");
+	window.console.log("包括记忆，也不会积存太久");
+	window.console.log("于是，倾其所能");
+	window.console.log("守住一座城，守住一个人");
+	window.console.log("守住一颗心，守住一段情");
+	window.console.log("一生相随，看尽人间细水长流");
+	window.console.log("不思量，自难忘");
+      window.console.log("%c对自己好点，因为一生不长；对身边的人好点，因为来生不一定遇见", "font-size:18px;color:red;");
+      window.console.log("%c洛璃 | localhost", "color:#fff; background: linear-gradient(270deg, #986fee, #8695e6, #68b7dd, #18d7d3); padding: 8px 15px; border-radius: 0 15px 0 15px")
+      window.console.log("%chttp://localhost"," text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:5em");
